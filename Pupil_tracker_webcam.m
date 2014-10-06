ba1bd1fe438bf2ca273fa2811ae6b5086335fc57
SANDEEP KONAM <sandeepkonam2020@gmail.com>
2014-10-07 01:11:26 +0530

    Webcam & user input

--- Pupil_tracker_webcam.m
+++ Pupil_tracker_webcam.m
@@ -14,49 +14,38 @@ clear all;
 clc;
 
 %% Extracting Frames
-%vidObj = VideoReader('video001.mp4');       
+  
 vidObj=videoinput('winvideo',1, 'YUY2_640x480');		% Storing the video as an object | input from camera/webcam
-%preview(vidObj)
-%lastFrame = read(vidObj, inf);
-%numFrames = vidObj.NumberOfFrames;
 numFrames = 5;								% Number of frames to be captured for analysis to be carried out
-
-for i=1:numFrames 								% Iterating pupil detection for every frame  
-    %c_initial = clock;							% Measuring the time required to execute the program; add on
-    %gr=rgb2gray(read(vidObj,i));
-    %cropped = imresize(gr, 0.4);					% Incase, the processing time is high, image can be resized 
-    %cropped = imcrop(cropped,[80 10 350 350]);		% Can be cropped if noise or unnecessary objects exist
-    im=getsnapshot(vidObj);		
-    cropped = im;
-    imshow(cropped);							% Display present frame on which detected circle is drawn
-
-    bw = im2bw(cropped,0.225);					% Thresholding the input frame
-    [centersDark, radiiDark] = imfindcircles(bw,[15 30],'ObjectPolarity','dark');		% Approximating pupil to circle ;  deducing centers and radius 
-    if isempty(radiiDark)							% Incase there is no pupil in the present image / eye blink
-        radii(i)= 0;				
-    else						
-        radii(i)=radiiDark;							% appending all radii to a list
-    end
-
-    viscircles(centersDark, radiiDark,'EdgeColor','b');		% Drawing circles for the approximated pupil
-    %imwrite(cropped,strcat('F:\op2\',num2str(i),'.jpg'));  	% Incase the frames are to be re-checked
-    % imwrite(bw,strcat('F:\op2\',num2str(i),'bw.jpg'));
-
-    %{						
-    c_final = clock;				 
-    time=c_final-c_initial;
-    time_final = 3600*time(4)+60*time(5)+time(6);
-    time_final 
-    %}
-end
+figure
 
 for i=1:numFrames 								% X-axis for the plot /  time instants
 X(i)=i;
 end
+   
+for i=1:numFrames 								% Iterating pupil detection for every frame  
+    
+    cropped=getsnapshot(vidObj);	
+       
+    subplot(2, 1, 1);
+    imshow(cropped);							% Display present frame on which detected circle is drawn
+       
+    bw = im2bw(cropped,0.225);					% Thresholding the input frame
+    [centersDark, radiiDark] = imfindcircles(bw,[15 30],'ObjectPolarity','dark');		% Approximating pupil to circle ;  deducing centers and radius 
+     
+    if isempty(radiiDark)							% Incase there is no pupil in the present image / eye blink
+        radii(i)= 0;				
+    else						
+        radii(i)=radiiDark(1);							% appending all radii to a list
+    end
+    
+    viscircles(centersDark,radii(i),'EdgeColor','b');		% Drawing circles for the approximated pupil
+    
+    subplot(2,1,2);
+    plot(X,radii(i));
+end
 
-plot(X,radii);									% Plot the graph for time instants vs radii
-
-
-
+subplot(2,1,2);
+plot(X,radii);	
 
 
