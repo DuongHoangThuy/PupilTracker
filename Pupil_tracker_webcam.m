eb25db4a223f3f09f7016022c683b760bab5cc83
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-20 20:34:24 +0530

    made shitloads of comments changes

--- Pupil_tracker_webcam.m
+++ Pupil_tracker_webcam.m
@@ -1,13 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 																 
 %    Aim : Pupil detection and diameter plot							 
-%    Authors : Sandeep Konam, Dhruv Joshi											 
-%    Acknowledgements : Sujeath Pareddy			 
+%    Authors : Dhruv Joshi											 
+%    Acknowledgements : Sujeath Pareddy, Sandeep Konam
 %    Organization : Srujana - Center for Innovation, LVEPI						 	 
 %																 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Approach :: Extracting frames -> Thresholding the frame at hand  -> Circle Detection(Pupil Approximation) -> Radius measurement -> Plotting the graph
+% Approach :: Extracting frames -> Thresholding the frame at hand  -> cropping out excess -> Circle Detection(Pupil Approximation) -> Radius measurement -> Plotting the graph
 
 close all;
 clear all;
@@ -25,16 +25,18 @@ hold on;										% image will persist
 closeflag = 1;                                  % for now this doesn't really do anythng
 
 while(closeflag)                                % infinite loop
-    %% first we acquire the feed
+    %% first we acquire the feed and crop out unrequired parts to speed it all up
     acquired_snapshot = getsnapshot(vid);       % acquire single image from feed
-    cropped_snapshot = imcrop(acquired_snapshot,[85 50 140 112]);   % crop it out so that you can see just the center
+    cropped_snapshot = imcrop(acquired_snapshot,[85 50 140 112]);   % crop it out so that you can see just the center ref: http://www.mathworks.in/help/images/ref/imcrop.html
     subplot(1,2,1), imshow(cropped_snapshot);  % normal camera (greyscale)
     
     %% Then we threshold it to some value of threshold to be able to get the pupil out
     thresholded_image = im2bw(cropped_snapshot,0.37);   % threshold karo... this value has been obtained after playing around
     subplot(1,2,2), imshow(thresholded_image);  % display the image
+        
+    %% next we extract circles from this baby...
+    [centers, radii] = imfindcircles(thresholded_image,[10 50],'ObjectPolarity','dark');
+    disp(centers);
+    
     pause(0.001);                               % much less than 30 fps. wihtout this it doesn't seem to work
-    
-    %% next we want to be able to crop out the unnecessary part of the image, keep only the center so we can perform a circle search...
-    
 end% Preview
