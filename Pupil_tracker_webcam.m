c41242d0dae4a263d71c58d9083fbf3832ce18ba
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-20 21:23:53 +0530

    removed the greyscale subplot, it's mucho faster now and possibly realtime

--- Pupil_tracker_webcam.m
+++ Pupil_tracker_webcam.m
@@ -8,6 +8,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Approach :: Extracting frames -> Thresholding the frame at hand  -> cropping out excess -> Circle Detection(Pupil Approximation) -> Radius measurement -> Plotting the graph
+% to be able to see the two feeds (greyscale and thresholded) simultaneously, uncomment the subplot part.
 
 close all;
 clear all;
@@ -28,18 +29,20 @@ while(closeflag)                                % infinite loop
     %% first we acquire the feed and crop out unrequired parts to speed it all up
     acquired_snapshot = getsnapshot(vid);       % acquire single image from feed
     cropped_snapshot = imcrop(acquired_snapshot,[85 50 140 112]);   % crop it out so that you can see just the center ref: http://www.mathworks.in/help/images/ref/imcrop.html
-    subplot(1,2,1), imshow(cropped_snapshot);  % normal camera (greyscale)
+    % subplot(1,2,1), imshow(cropped_snapshot);  % normal camera (greyscale)
     
     %% Then we threshold it to some value of threshold to be able to get the pupil out
     thresholded_image = im2bw(cropped_snapshot,0.37);   % threshold karo... this value has been obtained after playing around
-    subplot(1,2,2), imshow(thresholded_image);  % display the image
+    %subplot(1,2,2), 
+    imshow(thresholded_image);  % display the image
         
     %% next we extract circles from this baby...
     % from http://stackoverflow.com/questions/20400873/detect-a-circular-shape-inside-image-in-matlab we discover that imfindcircles() is insanely slow. There are better ways to find this object.
     % am increasing the sensitivity
-    [centers, radii] = imfindcircles(thresholded_image,[15 25], 'ObjectPolarity','dark','Sensitivity',0.94); 
-    if ~isempty(centers) 
-      subplot(1,2,2), viscircles(centers, radii,'EdgeColor','b');
+    [centers, radii] = imfindcircles(thresholded_image,[10 17], 'ObjectPolarity','dark','Sensitivity',0.94); 
+    if ~isempty(centers)                        % plot only if circle is detected.. ~ is logical not
+      %subplot(1,2,2), 
+      viscircles(centers, radii,'EdgeColor','b');
     end
     
     pause(0.001);                               % much less than 30 fps. wihtout this it doesn't seem to work
