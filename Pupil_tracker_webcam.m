e05babae411fb3819e4652ae7864195b99bc7632
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-24 15:17:19 +0530

    trying to get the graph to be a line plot

--- Pupil_tracker_webcam.m
+++ Pupil_tracker_webcam.m
@@ -17,6 +17,7 @@ clc;
 %% the following variables will be used to measure the time
 t1 = clock;
 t = 0;  
+pointsArray = [];                         % creating an appendable empty array
 
 vid = videoinput('winvideo', 1,'YUY2_320x240');          % Video Parameters
 
@@ -52,21 +53,22 @@ while(closeflag)                                % infinite loop
       % disp(radii(1))                          % just seeing radii range
       
       % all the plotting...
-      y = radii(1);                             % radii(1) is the first returned radius, converted to y-variable
+      y = radii(1);                              % radii(1) is the first returned radius, converted to y-variable
       t2 = clock;                               % finding out the time elapsed
       drawnow
       subplot(1,2,2);
       hold on;                                  % this will let us see the previous values also by rapidly auto-changing the x-axis i.e plot won't refresh new values will plotted on the same plot
-      pointsArray = [];                         % creating an appendable empty array
-      pointsArray = [pointsArray;[etime(t2,t1)*1000, y]];      % appending the array with the new entries
+      
+      pointsArray = [pointsArray;[etime(t2,t1)*1000, y]]      % appending the array with the new entries
        if t == 0
-         plot(pointsArray(t+1),pointsArray(t+1,2), '--o','linewidth',1.0),xlabel('time in 10ms'),ylabel('Pupil radius'); %pllotting the points by taking the value from the array
+         plot(pointsArray(t+1),pointsArray(t+1,2), 'linewidth',1.0),xlabel('time in 10ms'),ylabel('Pupil radius'); %pllotting the points by taking the value from the array
        end
        if t ~= 0 
-           plot(pointsArray(t+1),pointsArray(t+1,2), '--o','linewidth',1.0),xlabel('time in 10ms'),ylabel('Pupil radius');%should work on this part this should give lines
-           plot(pointsArray(t,:),pointsArray(t+1,:));
+           plot(pointsArray(t+1),pointsArray(t+1,2), 'linewidth',1.0),xlabel('time in 10ms'),ylabel('Pupil radius');%should work on this part this should give lines
+           % plot(pointsArray(t,:),pointsArray(t+1,:));
        end
+       t = t + 1;                               % t just counts the iterations
     end
-    
+   
     pause(0.001);                               % much less than 30 fps. wihtout this it doesn't seem to work
 end% Preview
