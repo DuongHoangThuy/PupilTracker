40edf55113cdcdcf72bdb226f3fa0c1681c98dd9
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-11-14 14:47:50 +0530

    yes the white LED is coming on added some comments to explain

--- exciteKaro.m
+++ exciteKaro.m
@@ -1,3 +1,5 @@
+% arguments need to be at the end, after source and callbackdata which are
+% fixed args: http://stackoverflow.com/questions/9078790/pushbutton-to-change-variable
 function exciteKaro(source,callbackdata, a)
   % the two arguments 'source' and 'callbackdata' are required to be
   % specified otherwise it doesn't work. taken from http://in.mathworks.com/help/matlab/ref/uicontrol.html
