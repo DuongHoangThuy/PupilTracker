b5947189eeb41f7ecef51884d8fa7fa3cb960bfe
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-11-15 10:12:34 +0530

    excitation of white LED now happens through serial comm and arduino

--- exciteKaro.m
+++ exciteKaro.m
@@ -1,19 +1,12 @@
 % arguments need to be at the end, after source and callbackdata which are
 % fixed args: http://stackoverflow.com/questions/9078790/pushbutton-to-change-variable
-function exciteKaro(source,callbackdata, a)
-  t3 = clock;       % get the time at the start yo
-  tnow = clock;
-  % the two arguments 'source' and 'callbackdata' are required to be
-  % specified otherwise it doesn't work. taken from http://in.mathworks.com/help/matlab/ref/uicontrol.html
-  %% first we put the white LED on...
-  white = 10;         % connected to PWM pin10
-  white_power = 0.1;
-  writePWMDutyCycle(a, white, white_power);
-  
-  %% next we wait for 5 seconds to pass.. and then close the LED..
-  while(etime(tnow, t3) < 5.00)
-    % do nothing.
-    tnow = clock;
-  end
-  writePWMDutyCycle(a, white, 0);
+% the two arguments 'source' and 'callbackdata' are required to be
+% specified otherwise it doesn't work. taken from http://in.mathworks.com/help/matlab/ref/uicontrol.html
+function exciteKaro(source,callbackdata)
+  % since asynchronous multithreaded work on MATLAB is not possible, we
+  % will simply use serial communication to arduino and write the functions
+  % on the arduino. http://arduino.stackexchange.com/questions/140/how-can-i-communicate-from-arduino-to-matlab
+  s = serial('COM10');      % serial communication object, change com port as needed
+  fopen(s);                 % open the serial port for comm
+  fprintf(s, 'w');          % single character to communicate white LED should be turned on*
 end
